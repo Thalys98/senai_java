a5c2bd06b6d525c4c167881f5ef6353e478bfa64
thalys <thalys55gabriel66@gmail.com>
2026-04-15 21:36:31 -0300

    exercicio1504

---
+++ Polimorfismo/src/model/Conta_corrente.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Conta_corrente {
+private String titular;
+private String agencia;
+private String numeroConta;
+private double saldo;
+ public Conta_corrente(String titlar,String agencia,String numeroConta,double saldo) {
+	 super();
+	 this.titular=titular;
+	 this.agencia=agencia;
+	 this.numeroConta=numeroConta;
+ }
+ public String getTitular()
+ }
+
