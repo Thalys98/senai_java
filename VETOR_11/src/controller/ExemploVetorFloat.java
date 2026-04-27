dd3e1563d6515ea48c023a76dbd0b1ded3720b03
thalys <thalys55gabriel66@gmail.com>
2026-04-27 18:17:42 -0300

    vetores

---
+++ VETOR_11/src/controller/ExemploVetorFloat.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class ExemploVetorFloat {
+	
+}
+	public static void main(String[] args) {
+	
---
+++ VETOR_11/src/controller/exemplovetorInt.java
@@ -0,0 +1,14 @@
+package controller;
+
+public class exemplovetorInt {
+
+	public static void main(String[] args) {
+	
+	int numeros []= new int [4];
+	numeros [0]=6;
+	numeros [2] = 17;
+	System.out.println("numeros[2]="+numeros[2]);
+	
+	}
+
+}
